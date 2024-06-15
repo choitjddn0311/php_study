72cf576037b70926d2f9a48ef6cf881338e97756
choitjddn0311 <cswcjw311427@naver.com>
2024-06-15 12:15:30 +0900

    feat : number operator

---
+++ num_operator.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learn number and num operator</title>
+</head>
+<body>
+    <h1>1+1</h1>
+    <?php
+        echo 1+1; //php에선 echo는 출력하는 거 print() 도 똑같음
+    ?>
+    <h1>2-1</h1>
+    <?php
+        echo 2-1;
+    ?>
+    <h1>2*2</h1>
+    <?php
+        echo 2*2;
+    ?>
+    <h1>4/2</h1>
+    <?php
+        echo 4/2;
+    ?>
+</body>
+</html>
