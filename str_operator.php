7fe75e67217ef070f122f8037e04652da592f422
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-06-16 23:58:02 +0900

    feat : add string operator

--- str_operator.php
+++ str_operator.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>String & String Operator</title>
 </head>
 <body>
-    
+    <h1>String & String Operator</h1>
+    <?php
+        echo "Hello \"w\"orld"; //역슬레쉬 안엔 아무역할을 안하게됨
+    ?>
+    <h2>concatenation operator</h2>
+    <?php 
+        echo "Hello" . "world" ; // . 은 좌항과 우항을 합쳐 하나에 문자열을 만듬
+    ?>
+    <h2>String length function</h2>
+    <?php
+        echo strlen("Hello world"); 
+    ?>
 </body>
 </html>
