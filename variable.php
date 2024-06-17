b87498970e593d3610a4ef730b1e17b30f17d4e6
choitjddn0311 <cswcjw311427@naver.com>
2024-06-17 19:15:47 +0900

    feat : add variable

---
+++ variable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+</head>
+<body>
+    <h1>Variable</h1>
+    <?php
+        $name = "노노무무현현";
+        echo "Lorem ipsum dolor sit ".$name." consectetur adipisicing elit. Dolore dolorem natus vero. Delectus ".$name." sequi enim ipsa voluptas inventore maxime. Sapiente repudiandae maxime ullam id neque totam dolorum libero debitis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit corporis quam repellat nostrum quasi hic ex consequatur nam illo. Reiciendis fugiat obcaecati voluptatibus nostrum. Repellat officiis voluptatem cumque error unde?";
+    ?>
+</body>
+</html>
