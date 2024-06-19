7131d6034e1670c1f59e5d74ca1143c754ff2386
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-06-19 23:15:22 +0900

    feat : php parameter get

---
+++ parameter.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>parameter</title>
+</head>
+<body>
+    안녕하세요. <?php echo $_GET['address']; ?>에 사시는 <?php echo $_GET['name']?>님 <!-- url로 넘어온 name의 값-->
+</body>
+</html>
