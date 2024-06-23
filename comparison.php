a370a9e539ecc574dca83e45e88f4d7df754a07a
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2024-06-23 23:04:04 +0900

    feat : boolean study

---
+++ comparison.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Comparison Operators &amp; Boolean data type</h1>
+    <h2>1==1</h2>
+    <?php
+        //type확인 함수 
+        var_dump(1==1);
+    ?>
+    <h2>1>1</h2>
+    <?php
+        var_dump(1>1);
+    ?>
+    <h2>1>=1</h2>
+    <?php
+        var_dump(1>=1);
+    ?>
+</body>
+</html>
